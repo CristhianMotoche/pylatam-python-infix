3950437b413d93c5b12077cbf84c80103166cd39
CristhianMotoche <cristhian.motoche@gmail.com>
2022-08-20 16:44:04 -0500

    Infix class example

--- infix.py
+++ infix.py
@@ -6,3 +6,20 @@
 #
 # div 8 2 == 4
 # 8 `div` 2 == 4
+
+
+
+class Infix(object):
+    def __init__(self, function):
+        self.function = function
+
+    def __ror__(self, other):
+        return Infix(lambda x: self.function(other, x))
+
+    def __or__(self, other):
+        return self.function(other)
+
+
+prod = Infix(lambda x, y: x * y)
+
+print(3 |prod| 3)
