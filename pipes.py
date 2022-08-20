eb7cb2dc29db5e50207298909376f1df12a772c5
CristhianMotoche <cristhian.motoche@gmail.com>
2022-08-20 16:09:20 -0500

    Add example for pipes

--- pipes.py
+++ pipes.py
@@ -5,3 +5,30 @@
 #   |> fun1
 #   |> fun2(x, y)
 #   |> fun3
+
+class Pipe:
+    def __init__(self, fn):
+        self.fn = fn
+
+    # arg | pipe == pipe.__ror__(arg)
+    def __ror__(self, arg):
+        return self.fn(arg)
+
+    def __call__(self, *args, **kwargs):
+        return Pipe(lambda x: self.fn(x, *args, **kwargs))
+
+value = [1,2,3]
+iddy = Pipe(lambda x: x)
+length = Pipe(lambda ls: len(ls))
+select = Pipe(lambda iterable, fn: map(fn, iterable))
+to_list = Pipe(list)
+
+print("*"*30)
+print(
+  value
+  | iddy
+  | select(lambda x: x ** 5)
+  | select(lambda x: x + 2)
+  | select(lambda x: x - 3)
+  | to_list
+)
